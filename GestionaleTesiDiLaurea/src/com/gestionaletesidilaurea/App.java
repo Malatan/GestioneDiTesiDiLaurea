b98356fdf4f6775358b2e3bef13647a832f31711
williamzhao1999 <william.zhao@stud.unifi.it>
2022-01-25 20:50:11 +0100

    aggiunto label matricola password e il bottone login

--- GestionaleTesiDiLaurea/src/com/gestionaletesidilaurea/App.java
+++ GestionaleTesiDiLaurea/src/com/gestionaletesidilaurea/App.java
@@ -6,8 +6,12 @@ import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.widgets.Label;
 
 public class App {
+	private static Text text;
+	private static Text text_1;
 
 	/**
 	 * Launch the application.
@@ -15,22 +19,36 @@ public class App {
 	 */
 	public static void main(String[] args) {
 		Display display = Display.getDefault();
-		Shell shell = new Shell();
-		shell.setSize(450, 300);
-		shell.setText("SWT Application");
+		Shell shlGestionaleDiTesi = new Shell();
+		shlGestionaleDiTesi.setSize(509, 317);
+		shlGestionaleDiTesi.setText("Gestionale di tesi di laurea");
 		
-		Button btnNewButton = new Button(shell, SWT.NONE);
+		Button btnNewButton = new Button(shlGestionaleDiTesi, SWT.NONE);
 		btnNewButton.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
 			}
 		});
-		btnNewButton.setBounds(163, 204, 75, 25);
-		btnNewButton.setText("Hello");
+		btnNewButton.setBounds(158, 163, 184, 40);
+		btnNewButton.setText("Login");
+		
+		text = new Text(shlGestionaleDiTesi, SWT.BORDER);
+		text.setBounds(141, 68, 227, 21);
+		
+		text_1 = new Text(shlGestionaleDiTesi, SWT.BORDER);
+		text_1.setBounds(141, 111, 227, 21);
+		
+		Label lblMatricola = new Label(shlGestionaleDiTesi, SWT.NONE);
+		lblMatricola.setBounds(72, 68, 55, 15);
+		lblMatricola.setText("Matricola");
+		
+		Label lblPassword = new Label(shlGestionaleDiTesi, SWT.NONE);
+		lblPassword.setBounds(72, 111, 55, 15);
+		lblPassword.setText("Password");
 
-		shell.open();
-		shell.layout();
-		while (!shell.isDisposed()) {
+		shlGestionaleDiTesi.open();
+		shlGestionaleDiTesi.layout();
+		while (!shlGestionaleDiTesi.isDisposed()) {
 			if (!display.readAndDispatch()) {
 				display.sleep();
 			}
