34c04e6d38ce9dd1a004daa6618a09c6730cca0a
Malatan <matteozchn124@gmail.com>
2022-02-28 05:23:18 +0100

    modifiche per adattare nuova db, tolti metodi vuoti e aggiornamenti

--- GestionaleTesiDiLaurea/src/databaseAccessObject/Database.java
+++ GestionaleTesiDiLaurea/src/databaseAccessObject/Database.java
@@ -6,7 +6,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import com.mysql.cj.jdbc.exceptions.CommunicationsException;
+import java.util.ArrayList;
+
 import domainModel.*;
 import utils.*;
 
@@ -68,6 +69,7 @@ public class Database {
 				return false;
 		} catch (SQLException e) {
 			Console.print("Connessione al db fallita", "db");
+			e.printStackTrace();
 			return false;
 		}
 	}
@@ -77,45 +79,68 @@ public class Database {
 		try {
 			connection = DriverManager.getConnection(connectionString);
 			Statement stm = connection.createStatement();
-			Console.print("SELECT matricola, nome, cognome, ruolo from users "
-					+ "WHERE matricola = '" + matricola + "' AND password= '" + password +"'", "sql");
-			ResultSet rs = stm.executeQuery("SELECT matricola, nome, cognome, ruolo from users WHERE matricola = '" + matricola + "' AND password= '" + password +"'");
-			//System.out.println(rs.getString("cognome") + " " + rs.getString("nome") + " di ruolo " + rs.getInt("ruolo"));
-			
+			String query = "SELECT * from utente " + "WHERE matricola = '" + matricola + "' AND password= '" + password +"'";
+			Console.print(query, "sql");
+			ResultSet rs = stm.executeQuery(query);
 			if (rs.next()) {
 				String[] info = new String[4];
 				info[0] = rs.getString("matricola");
 				info[1] = rs.getString("nome");
-				info[2] = rs.getString("ruolo");
 				info[3] = rs.getString("cognome");
+				info[2] = rs.getString("ruolo");
 				return info;
 			}
 		} catch (SQLException e) {
-			System.out.println("2222222");
+			e.printStackTrace();
 		} 
 		return null;
 	}
 	
-	public Boolean registraIntentoPartecipazione() {
-		return true;
+	/*0 = non e' presenta la domanda, 
+	  1 = e' presente la domanda ma non ancora in appello,
+	  2 = e' stato assegnato ad un appello*/
+	public int getStatusTesi(int matricola) {
+		Connection connection = null;
+		try {
+			connection = DriverManager.getConnection(connectionString);
+			Statement stm = connection.createStatement();
+			String query = "SELECT * FROM DOMANDATESI WHERE MATRICOLA = " + matricola;
+			Console.print(query, "sql");
+			ResultSet rs = stm.executeQuery(query);
+			if (rs.next()) {
+				return 1;
+			} else {
+				return 0;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} 
+		return 0;
 	}
 	
-	public Boolean caricaTesi() {
-		return true;
-	}
-	
-	public Boolean rimuoviCandidato(String matricola) {
-		return true;
+	public void iscrizioneTesi(Studente studente, String data, int id_corso) {
+		Connection connection = null;
+		try {
+			connection = DriverManager.getConnection(connectionString);
+			String query = "insert into domandatesi (matricola, data, id_corso) values (?,?,?)";
+			PreparedStatement prepared = connection.prepareStatement(query);
+			prepared.setInt(1, studente.getMatricolaInt());
+			prepared.setString(2, data);
+			prepared.setInt(3, id_corso);
+			Console.print(prepared.toString(), "sql");
+			prepared.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} 
 	}
 	
 	public boolean aggiungiAppello(String data) {
 		Connection connection = null;
 		try {
 			connection = DriverManager.getConnection(connectionString);
-			String query = "insert into appelli (data, idPresidente) values (?,?)";
+			String query = "insert into appello (data) values (?)";
 			PreparedStatement prepared = connection.prepareStatement(query);
 			prepared.setString(1, data);
-			prepared.setInt(2, 0);
 			Console.print(prepared.toString(), "sql");
 			prepared.executeUpdate();
 		} catch (SQLException e) {
@@ -127,43 +152,26 @@ public class Database {
 	}
 	
 	public AppelloTesi[] getAppelli() {
-				
 		Connection connection = null;
 		try {
 			connection = DriverManager.getConnection(connectionString);
-			
 			Statement stm = connection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
-
-
-			ResultSet rs = stm.executeQuery("SELECT * from appelli as a LEFT JOIN aule as au ON a.idAula = au.id");
-			//System.out.println(rs.getString("cognome") + " " + rs.getString("nome") + " di ruolo " + rs.getInt("ruolo"));
-			
+			String query = "SELECT * from appello";
+			Console.print(query, "sql");
+			ResultSet rs = stm.executeQuery(query);
 			rs.last();
 			int rowsCount = rs.getRow();
 			rs.beforeFirst();
-			
 			AppelloTesi[] appelli = new AppelloTesi[rowsCount];
-			
 			int index = 0;
 			while (rs.next()) {
-				System.out.print(rs.getInt("idAppello") + " " +
-						rs.getString("data") + " " +
-						rs.getInt("idPresidente") + " " +
-						rs.getInt("idAula") + " " +
-						rs.getString("numAula"));
-				appelli[index] = new AppelloTesi(rs.getInt("idAppello"),
+				appelli[index] = new AppelloTesi(rs.getInt("id_appello"),
 						rs.getString("data"),
-						rs.getInt("idPresidente"),
-						rs.getInt("idAula"),
-						rs.getString("numAula")
-						);
+						rs.getString("nota"));
 				index++;
 			}
-			
-			
 			connection.close();
 			return appelli;
-
 		} catch (SQLException e) {
 			e.printStackTrace();
 			return null;
@@ -212,6 +220,26 @@ public class Database {
 			return null;
 		} 
 
+	}
+	
+	public ArrayList<Pair<Integer, String>> getCorsi() {
+		Connection connection = null;
+		ArrayList<Pair<Integer, String>> corsi = new ArrayList<Pair<Integer, String>>();
+		try {
+			connection = DriverManager.getConnection(connectionString);
+			Statement stm = connection.createStatement();
+			String query = "SELECT * FROM CORSO";
+			Console.print(query, "sql");
+			ResultSet rs = stm.executeQuery(query);
+			while (rs.next()) {
+				corsi.add(Pair.of(rs.getInt("id_corso"), 
+									rs.getString("nome"))
+				);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return corsi;
 	}
 	
 	public Boolean prenotaAula(int idAula, int idAppello, String currentAula) {
@@ -327,31 +355,5 @@ public class Database {
 		} 
 		return null;
 	}
-	
-	public Boolean validaComposizioneCommissione(Boolean accettato) {
-		return true;
-	}
-	
-	public Boolean salvaDate(String Appello) {
-		return true;
-	}
-	
-	public Boolean inserisciPresidenteCommissioneTesi(String matricola, String idAppello) {
-		return true;
-	}
-	
-	public Boolean validaPartecipazioneTesi(Boolean v) {
-		return true;
-	}
-	
-	public Boolean approvaTesi(Boolean appr) {
-		return true;
-	}
-	
-	public Boolean inserisciGiustificazione(String matricola, String note) {
-		return true;
-	}
-	
-
 	
 }
