6d2292c4f895b8516c1d86d269df37fa8c69840b
Malatan <matteozchn124@gmail.com>
2022-02-28 05:20:46 +0100

    Ristrutturato la database sql

--- gestionaletesi.sql
+++ gestionaletesi.sql
@@ -1,148 +1,126 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Creato il: Feb 26, 2022 alle 15:27
--- Versione del server: 10.4.22-MariaDB
--- Versione PHP: 7.4.27
+DROP DATABASE IF EXISTS gestionaletesi;
+CREATE DATABASE gestionaletesi;
+USE gestionaletesi;
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+-- 0 = studente, 1 = responsabile,  2 = presidente scuola, 3 = presidente corso
+-- 4 = docente
+CREATE TABLE utente(
+	matricola int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	nome varchar(50) NOT NULL,
+	cognome varchar(50) DEFAULT NULL,
+	password  varchar(50) NOT NULL,
+	ruolo int not NULL
+)AUTO_INCREMENT=10000;
 
+CREATE TABLE corso(
+	id_corso int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	nome varchar(50) NOT NULL,
+	presidente int NOT NULL,
+	FOREIGN KEY (presidente) REFERENCES utente(matricola)
+)AUTO_INCREMENT=20000;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+CREATE TABLE appello(
+	id_appello int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	data date DEFAULT NULL,
+	nota text DEFAULT NULL
+)AUTO_INCREMENT=30000;
 
---
--- Database: `gestionaletesi`
---
+CREATE TABLE dipartimento(
+	id_dipartimento int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	nome varchar(50) DEFAULT NULL
+)AUTO_INCREMENT=40000;
 
--- --------------------------------------------------------
+CREATE TABLE docente_dipartimento(
+	id_dipartimento int NOT NULL,
+	matricola int NOT NULL PRIMARY KEY,
+	FOREIGN KEY (id_dipartimento) REFERENCES dipartimento(id_dipartimento),
+	FOREIGN KEY (matricola) REFERENCES utente(matricola)
+);
 
---
--- Struttura della tabella `appelli`
---
+/*
+CREATE TABLE appello_studente(
 
-CREATE TABLE `appelli` (
-  `idAppello` int(11) NOT NULL,
-  `data` datetime NOT NULL,
-  `idPresidente` int(11) NOT NULL,
-  `idAula` int(11) DEFAULT NULL,
-  `informazioni` text DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+);
 
---
--- Dump dei dati per la tabella `appelli`
---
+CREATE TABLE appello_docente(
 
-INSERT INTO `appelli` (`idAppello`, `data`, `idPresidente`, `idAula`, `informazioni`) VALUES
-(6, '1999-12-13 14:13:00', 0, 8, 'L\'appello si svolgerà il 13/01/2000 alle ore 13:12 Link meet: https://....com'),
-(7, '1999-12-13 14:14:00', 0, NULL, NULL),
-(8, '1999-12-13 14:15:00', 0, NULL, NULL);
+);*/
 
--- --------------------------------------------------------
+CREATE TABLE domandatesi(
+	matricola int NOT NULL,
+	data date NOT NULL,
+	id_corso int NOT NULL,
+	FOREIGN KEY (matricola) REFERENCES utente(matricola),
+	FOREIGN KEY (id_corso) REFERENCES corso(id_corso)
+);
 
---
--- Struttura della tabella `aule`
---
+-- popolamento utente
+INSERT INTO utente (nome, cognome, password, ruolo)
+VALUES("Tizio", "Caio", 123, 0), 
+("Riccardo", "Cremonesi", 123, 0), 
+("Gianni", "Manfrin", 123, 1),
+("Raniero", "Calabrese", 123, 2), 
+("Libero", "Siciliano", 123, 3), 
+("Gofreddo", "Lombardi", 123, 3), 
+("Delfino", "Pirozzi", 123, 3), 
+("Brancaleone", "Lucciano", 123, 3), 
+("Biagio", "Bellucci", 123, 3), 
+("Liberato", "Zetticci", 123, 4), 
+("Cristiano", "Calabresi", 123, 4), 
+("Marino", "Lorenzo", 123, 4), 
+("Eugenio", "Nucci", 123, 4), 
+("Marzio", "Milanesi", 123, 4), 
+("Valentino", "Sabbatini", 123, 4), 
+("Baldassarre", "Lucciano", 123, 4),
+("Romeo", "Dellucci", 123, 4),
+("Alvise", "Pagnotto", 123, 4),
+("Pio", "Lori", 123, 4),
+("Carola", "Davide", 123, 4),
+("Geronimo", "Bianchi", 123, 4),
+("Lino", "Toscano", 123, 4),
+("Lino", "Bellucci", 123, 4),
+("Caton", "Toscano", 123, 4),
+("Toscano", "Rossi", 123, 4),
+("Beato", "Romani", 123, 4),
+("Beato", "Dellucci", 123, 4),
+("Cino", "Mancini", 123, 4),
+("Cino", "Esposito", 123, 4);
 
-CREATE TABLE `aule` (
-  `id` int(11) NOT NULL,
-  `numAula` varchar(512) NOT NULL,
-  `libera` int(11) NOT NULL DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+-- popolamento corso
+INSERT INTO corso (nome, presidente)
+VALUES("Biomedica", 10004), 
+("Elettronica", 10005), 
+("Informatica", 10006), 
+("Meccanica", 10007), 
+("Edile", 10008);
 
---
--- Dump dei dati per la tabella `aule`
---
+-- popolamento dipartimento
+INSERT INTO dipartimento (nome)
+VALUES("Dipartimento di Medicina Sperimentale e Clinica"), 
+("Dipartimento di Architettura"), 
+("Dipartimento di Ingegneria dell'Informazione"), 
+("Dipartimento di Ingegneria Industriale"), 
+("Dipartimento di Biologia");
 
-INSERT INTO `aule` (`id`, `numAula`, `libera`) VALUES
-(1, '1', 1),
-(2, '2', 1),
-(5, '3', 1),
-(6, '4', 1),
-(7, '100', 1),
-(8, 'Auditorium A', 0),
-(9, '102', 1),
-(10, '103', 1);
-
--- --------------------------------------------------------
-
---
--- Struttura della tabella `users`
---
-
-CREATE TABLE `users` (
-  `matricola` int(11) NOT NULL,
-  `nome` varchar(512) NOT NULL,
-  `cognome` varchar(512) NOT NULL,
-  `password` varchar(512) NOT NULL,
-  `ruolo` int(11) NOT NULL DEFAULT 0 COMMENT '0 = studente, 1 = responsabile, 2 = presidente scuola, 3= presidente corso, 4 =relatore, 5 =membrocomm, 6 = presidentecommissione'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dump dei dati per la tabella `users`
---
-
-INSERT INTO `users` (`matricola`, `nome`, `cognome`, `password`, `ruolo`) VALUES
-(1, 'Tizio', 'Tizio', 'test', 1),
-(2, 'Caio', 'Caio', 'test', 2),
-(3, 'Senpronio', 'Senpronio', 'test', 3),
-(4, 'Mario', 'Rossi', 'test', 4),
-(5, 'Will', 'Smith', 'test', 5),
-(6, 'Test', 'Test', 'test', 6),
-(22, 'Chaohao', 'Zheng', 'test', 0),
-(25, 'William', 'Zhao', 'test', 0);
-
---
--- Indici per le tabelle scaricate
---
-
---
--- Indici per le tabelle `appelli`
---
-ALTER TABLE `appelli`
-  ADD PRIMARY KEY (`idAppello`);
-
---
--- Indici per le tabelle `aule`
---
-ALTER TABLE `aule`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indici per le tabelle `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`matricola`);
-
---
--- AUTO_INCREMENT per le tabelle scaricate
---
-
---
--- AUTO_INCREMENT per la tabella `appelli`
---
-ALTER TABLE `appelli`
-  MODIFY `idAppello` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-
---
--- AUTO_INCREMENT per la tabella `aule`
---
-ALTER TABLE `aule`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
-
---
--- AUTO_INCREMENT per la tabella `users`
---
-ALTER TABLE `users`
-  MODIFY `matricola` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- docente-dipartimento
+INSERT INTO docente_dipartimento (id_dipartimento, matricola)
+VALUES(40000, 10009),
+(40000, 10010),
+(40000, 10011),
+(40000, 10012),
+(40001, 10013),
+(40001, 10014),
+(40001, 10015),
+(40001, 10016),
+(40002, 10017),
+(40002, 10018),
+(40002, 10019),
+(40002, 10020),
+(40003, 10021),
+(40003, 10022),
+(40003, 10023),
+(40003, 10024),
+(40004, 10025),
+(40004, 10026),
+(40004, 10027),
+(40004, 10028);
